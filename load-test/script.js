5df97c99ad3f8e800162beb03a4ea245235c283c
Paulo Cândido <paulo.candido@g.globo>
2023-02-12 23:05:32 -0300

    feat: adjust load test

--- load-test/script.js
+++ load-test/script.js
@@ -1,16 +1,18 @@
 import http from 'k6/http'
-import { check, sleep } from 'k6';
+import { check, sleep } from 'k6'
 
 export const options = {
   stages: [
-    { duration: '30s', target: 1 },
-    { duration: '2m', target: 10 },
+    { duration: '30s', target: 2 },
+    { duration: '10s', target: 30 },
+    { duration: '2m', target: 30 },
+    { duration: '10s', target: 2 },
     { duration: '2m', target: 2 },
   ],
 }
 
 export default function () {
-  const res = http.get('http://api:3000/')
+  const res = http.get('http://api:3000/', { timeout: '2s' })
   check(res, { 'status was 200': (r) => r.status == 200 })
-  sleep(20)
+  sleep(0.5)
 }
