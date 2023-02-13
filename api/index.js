089dc1f93b803060923716bb360d8d1a9a7c9441
Paulo Cândido <paulo.candido@g.globo>
2023-02-12 23:05:14 -0300

    fix: replace setTimeout to a real load simulator

--- api/index.js
+++ api/index.js
@@ -6,17 +6,17 @@ process.on('SIGINT', () => {
 
 const app = new express()
 const port = process.env.PORT || 3000
-const maxProcessingTime = process.env.MAX_TIME || 1000
 
 app.get('/healthcheck', (req, res) => {
   res.status(200).send('OK')
 })
 
 app.get('/', (req, res) => {
-  const timeToProcess = Math.round(Math.random() * maxProcessingTime)
-  setTimeout(() => {
-    res.status(200).send(`Hello World. Processed in ${timeToProcess}ms`)
-  }, timeToProcess)
+  const start = new Date().getTime()
+  for(let i = 0; i< 500000000; i++){} // simulate load
+  const end = new Date().getTime()
+
+  res.status(200).send(`Hello World. Processed in ${end - start}ms`)
 })
 
 app.listen(port, () => {
